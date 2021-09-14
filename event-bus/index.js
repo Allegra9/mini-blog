072979bd1fa01d115fb322b166a2f91aa8fcc8f3
Allegra9 <aukseallegra@gmail.com>
2021-09-14 19:59:29 +0100

    Add super basic event-bus
    Receive events and post them to all other services

---
+++ event-bus/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const axios = require('axios')
+
+const app = express()
+app.use(express.json())
+
+const sendEvent = (url, payload) => {
+  axios.post(url, payload).catch(err => console.log(err.message))
+}
+
+app.post('/events', (req, res) => {
+  const event = req.body
+
+  sendEvent('http://localhost:4000/events', event)
+  sendEvent('http://localhost:4001/events', event)
+  // makeRequest('http://localhost:4002/events', event)
+
+  res.send({ status: 'OK' })
+})
+
+const PORT = 4005
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`)
+})
