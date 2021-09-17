b8ba2f88645f29abb634d9689b791517df1d2b55
Allegra9 <aukseallegra@gmail.com>
2021-09-17 17:55:39 +0100

    Store events, add endpoint to get all events
    This way any service can get events it missed on and needs to process.

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,6 +4,8 @@ const axios = require('axios')
 const app = express()
 app.use(express.json())
 
+const events = []
+
 const sendEvent = (url, payload) => {
   axios.post(url, payload).catch(err => console.log(err.message))
 }
@@ -11,12 +13,18 @@ const sendEvent = (url, payload) => {
 app.post('/events', (req, res) => {
   const event = req.body
 
+  events.push(event)
+
   sendEvent('http://localhost:4000/events', event)
   sendEvent('http://localhost:4001/events', event)
   sendEvent('http://localhost:4002/events', event)
   sendEvent('http://localhost:4003/events', event)
 
   res.send({ status: 'OK' })
+})
+
+app.get('/events', (req, res) => {
+  res.send(events)
 })
 
 const PORT = 4005
